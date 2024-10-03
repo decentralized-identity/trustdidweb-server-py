51eaf6d9cf0fe1d0735fa64f76117c1dbb5a3d4d
jamshale <jamiehalebc@gmail.com>
2024-10-03 15:10:26 -0700

    Move server to poetry
    Signed-off-by: jamshale <jamiehalebc@gmail.com>

--- server/app/routers/tdw.py
+++ server/app/routers/tdw.py
@@ -1,13 +1,3 @@
-from fastapi import APIRouter, Depends, HTTPException
-from fastapi.responses import JSONResponse
-from app.models.web_requests import RegisterDID
-from config import settings
-from app.plugins import AskarVerifier, AskarStorage, TrustDidWeb
-from app.dependencies import (
-    identifier_available,
-    did_document_exists,
-    valid_did_registration,
-)
-from app.utilities import create_did_doc_template
+from fastapi import APIRouter
 
 router = APIRouter()

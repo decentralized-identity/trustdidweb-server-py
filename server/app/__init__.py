7d6d49564e732df9a9faaa696f8d8e6915717d6e
PatStLouis <patrick.st-louis@opsecid.ca>
2024-10-04 16:24:02 +0000

    updated dependencies and reformatting
    Signed-off-by: PatStLouis <patrick.st-louis@opsecid.ca>

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -7,9 +7,11 @@ app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
 
 api_router = APIRouter()
 
+
 @api_router.get("/server/status", tags=["Server"], include_in_schema=False)
 async def server_status():
     return JSONResponse(status_code=200, content={"status": "ok"})
+
 
 api_router.include_router(identifiers.router)
 

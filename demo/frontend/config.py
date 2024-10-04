b6e051121eb1bc009aeb55918483fb928dd0d3f2
PatStLouis <patrick.st-louis@opsecid.ca>
2024-10-04 16:14:03 +0000

    change demo config
    Signed-off-by: PatStLouis <patrick.st-louis@opsecid.ca>

--- demo/frontend/config.py
+++ demo/frontend/config.py
@@ -6,6 +6,6 @@ class Config(object):
     TESTING = True
     SECRET_KEY = "s3cret"
     
-    AGENT_ADMIN_URL = "https://agent.dev.opsec.id"
-    TDW_SERVER_URL = "https://trust.dev.opsec.id"
-    TDW_ENDORSER_MULTIKEY = "z6Mkiw1uyDmZ2cX872XhziSNm47Z3WV9EZqgqEH6grrLbSZ6"
+    AGENT_ADMIN_URL = "http://agent:8020"
+    TDW_SERVER_URL = "http://server:8000"
+    TDW_ENDORSER_MULTIKEY = "z6MkgKA7yrw5kYSiDuQFcye4bMaJpcfHFry3Bx45pdWh3s8i"

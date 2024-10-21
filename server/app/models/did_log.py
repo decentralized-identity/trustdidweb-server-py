e431fd7bdfb4550e8269ecccb46e838f04a672f3
PatStLouis <patrick.st-louis@opsecid.ca>
2024-10-21 19:24:52 +0000

    add witness object
    Signed-off-by: PatStLouis <patrick.st-louis@opsecid.ca>

--- server/app/models/did_log.py
+++ server/app/models/did_log.py
@@ -9,12 +9,23 @@ class BaseModel(BaseModel):
         return super().model_dump(by_alias=True, exclude_none=True, **kwargs)
 
 
+class Witness(BaseModel):
+    id: str = Field(None)
+    weight: int = Field(None)
+
+
+class WitnessParam(BaseModel):
+    threshold: int = Field(None)
+    selfWeight: int = Field(None)
+    witnesses: List[Witness] = Field(None)
+
+
 class LogParameters(BaseModel):
     prerotation: bool = Field(None)
     portable: bool = Field(None)
     updateKeys: List[str] = Field(None)
     nextKeyHashes: List[str] = Field(None)
-    witness: List[str] = Field(None)
+    witness: WitnessParam = Field(None)
     deactivated: bool = Field(None)
     ttl: bool = Field(None)
     method: str = Field(None)
